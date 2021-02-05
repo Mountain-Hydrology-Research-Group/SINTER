cf6d110b1a24e0f0263e7ff206f7fd69acb376f2
joricarter <joripwc@gmail.com>
2021-02-05 13:06:12 -0800

    updated the css for the logo discr between mac and windows

---
+++ JavaScript/index.js
@@ -0,0 +1,66 @@
+/*
+This is the JS for the SINTER pages. Please see the comments for the different types of functions that are being used here. The code is in
+the same order as the pages order on the menu, so the landing page functions are first, then the about us, etc.
+
+Jori Carter
+*/
+
+
+//Menu functions
+let menu = document.getElementById("myMenu");
+let main = document.getElementById("main");
+
+const openNav = () => {
+    menu.style.width = "250px";
+    main.style.marginLeft = "250px";
+    main.style.opacity = "0.5";
+    //document.body.style.overflow = none;
+};
+  
+const closeNav = () => {
+    menu.style.width = "0";
+    main.style.marginLeft = "0";
+    main.style.opacity = "1";
+    //document.body.style.overflow = hidden;
+};
+
+//SlideShow Functions
+let index = 0;
+
+const changingPicture = (index) => {
+    let picture = document.getElementById("pictureSlide");
+    let credits = document.getElementById("photoCredits");
+
+    if (index === 0) {
+        picture.src = "src/Sinter Snowmobile Picture.jpg";
+        credits.innerHTML = "Photo Credits: Arvids Silis"
+    } else if (index === 1) {
+        picture.src = "src/DSC_0416-1.jpg";
+        credits.innerHTML = "Photo Credits: Jori Carter";
+    } else if (index === 2) {
+        picture.src = "src/Igloo_lights[8748].jpg";
+        credits.innerHTML = "Photo Credits: Arvids Silis";
+    }
+}
+
+
+const automaticSlideShow = () => {
+    setInterval(increaseIndex(), 2000);
+}
+
+const increaseIndex = () => {
+    index++;
+    if (index > 2) {
+        index = 0;
+    }
+    changingPicture(index);
+}
+
+const decreaseIndex = () => {
+    index--;
+    if (index < 0) {
+        index = 2;
+    }
+    changingPicture(index);
+}
+//Snow School Page Functions
